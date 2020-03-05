f98695cc5fcd6552813326ec4ef7cfc3204af5c3
Carlos Chaguendo <carlos.chaguendo@hotmail.com>
2020-03-05 13:18:36 -0500

    Initial Commit

---
+++ Pneumonia/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Pneumonia
+//
+//  Created by Carlos Chaguendo on 5/03/20.
+//  Copyright © 2020 FUP. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pneumonia/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Pneumonia
+//
+//  Created by Carlos Chaguendo on 5/03/20.
+//  Copyright © 2020 FUP. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
